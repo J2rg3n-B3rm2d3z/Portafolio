d95b130d6a41868de8748b7cd5bb896a0b5d639a
Jurgen hace programas. <jurgenfbermudezpicado@gmail.com>
2024-08-08 01:34:08 -0600

    Deleted aboutme screen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { AboutMeComponent } from './about-me/about-me.component';
 import { ContactComponent } from './contact/contact.component';
 import { EducationComponent } from './education/education.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
